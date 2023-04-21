953c2bc526191bfd4ac23ce213e187f0cead2f69
Douglas Rodrigues <123272282+Zatanaris@users.noreply.github.com>
2023-04-21 14:01:35 -0300

    correção no codigo

--- interactive-rating-component-main/assets/js/main.js
+++ interactive-rating-component-main/assets/js/main.js
@@ -1,8 +1,24 @@
 const questio = document.querySelector(".Question--card");
 const result = document.querySelector(".result--card");
 const btn = document.querySelector(".btn");
+const options = document.querySelectorAll("[data-rating-btn]");
+const resultValue = document.getElementById("numero");
+
+let lestResultValue = NaN;
 
 btn.addEventListener("click", () => {
-    questio.classList.toggle("hide");
-    result.classList.toggle("hide");
+    if (lestResultValue) {
+        questio.classList.add("hide");
+        result.classList.remove("hide");
+        resultValue.textContent = lestResultValue;
+    }
 });
+
+for (let i = 0, len = options.length; i < len; i++) {
+    options[i].addEventListener("click", function () {
+        if (lestResultValue) options[lestResultValue - 1].classList.remove("active");
+
+        lestResultValue = Number(this.textContent);
+        this.classList.add("active");
+    });
+}
